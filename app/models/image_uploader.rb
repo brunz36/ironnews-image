98c2c4382e21e02c3ea8856692ececd28db91f5a
Bruno Ribeiro <brunz36@gmail.com>
2017-04-12 18:20:18 -0400

    Able to add images

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,3 +1,5 @@
+require "image_processing/mini_magick"
+
 class ImageUploader < Shrine
   # plugins and uploading logic
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ require 'nokogiri'
 user_1 = User.create(name: "Patrick Miller", email: "pjmiller823@gmail.com", password: "password", password_confirmation: "password")
 user_2 = User.create(name: "Bruno Ribeiro", email: "brunz36@gmail.com", password: "test", password_confirmation: "test")
 
-(1..10).each do |page|
+(1..2).each do |page|
   Nokogiri::HTML(open("https://news.ycombinator.com?p=#{page}")).css(".storylink").each_with_index { |article, index| Story.create(title: article.content, url_link: article.values.first) }
 end
